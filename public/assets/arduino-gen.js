487cb6344034bd910983ef73123705bb34be44f8
LorisWittkowski <151879670+LorisWittkowski@users.noreply.github.com>
2025-08-31 02:35:13 +0200

    Update arduino-gen.js

--- public/assets/arduino-gen.js
+++ public/assets/arduino-gen.js
@@ -3,7 +3,10 @@
 // Pins werden NICHT hier editiert – nur Anzeige & Generierung.
 
 (function(){
-  const ALLOWED_PINS = ["A0","A1","A2","A3","A4","A5"];
+    const ALLOWED_PINS = [
+      "A0","A1","A2","A3","A4","A5",
+      "A6","A7","A8","A9","A10","A11","A12","A13"
+    ];
 
   // ---------- helpers ----------
   async function fetchJSON(url){
